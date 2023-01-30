fe09b11e27755e5deb1b91a9cce10b15bcede051
brichen95 <brichen95@gmail.com>
2023-01-30 20:19:55 +0000

    Cleaning up

--- main.tf
+++ main.tf
@@ -3,46 +3,46 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source  = "telmate/proxmox"
       version = "2.9.11"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url = var.proxmox_api_url
-  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
   pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure = true # I don't have proxmox https/SSL set up right
+  pm_tls_insecure     = true # I don't have proxmox https/SSL set up right
 }
 
 resource "proxmox_vm_qemu" "kube-control" {
-  count = var.kube_control_count
-  name = "kube-control-0${count.index + 1}"
+  count       = var.kube_control_count
+  name        = "kube-control-0${count.index + 1}"
   target_node = var.proxmox_host
-  vmid = "80${count.index + 1}"
-  clone = var.template_name
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = var.kube_control_memory
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
+  vmid        = "80${count.index + 1}"
+  clone       = var.template_name
+  agent       = 1
+  os_type     = "cloud-init"
+  cores       = var.kube_control_cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.kube_control_memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
   disk {
-    slot = 0
-    size = var.kube_control_disksize
-    type = "scsi"
-    storage = "local"
+    slot     = 0
+    size     = var.kube_control_disksize
+    type     = "scsi"
+    storage  = "local"
     iothread = 0
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr17"
   }
   lifecycle {
@@ -52,36 +52,36 @@ resource "proxmox_vm_qemu" "kube-control" {
   }
   ipconfig0 = "ip=10.0.1.3${count.index + 1}/24,gw=10.0.1.1"
   ipconfig1 = "ip=10.17.0.3${count.index + 1}/24"
-  sshkeys = join("\n", var.ssh_keys)
+  sshkeys   = join("\n", var.ssh_keys)
 }
 
 resource "proxmox_vm_qemu" "kube-node" {
-  count = var.kube_node_count
-  name = "kube-node-0${count.index + 1}"
+  count       = var.kube_node_count
+  name        = "kube-node-0${count.index + 1}"
   target_node = var.proxmox_host
-  vmid = "90${count.index + 1}"
-  clone = var.template_name
-  agent = 1
-  os_type = "cloud-init"
-  cores = 2
-  sockets = 1
-  cpu = "host"
-  memory = var.kube_node_memory
-  scsihw = "virtio-scsi-pci"
-  bootdisk = "scsi0"
+  vmid        = "90${count.index + 1}"
+  clone       = var.template_name
+  agent       = 1
+  os_type     = "cloud-init"
+  cores       = var.kube_node_cores
+  sockets     = 1
+  cpu         = "host"
+  memory      = var.kube_node_memory
+  scsihw      = "virtio-scsi-pci"
+  bootdisk    = "scsi0"
   disk {
-    slot = 0
-    size = var.kube_node_disksize
-    type = "scsi"
-    storage = "local"
+    slot     = 0
+    size     = var.kube_node_disksize
+    type     = "scsi"
+    storage  = "local"
     iothread = 0
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr17"
   }
   lifecycle {
@@ -91,7 +91,7 @@ resource "proxmox_vm_qemu" "kube-node" {
   }
   ipconfig0 = "ip=10.0.1.4${count.index + 1}/24,gw=10.0.1.1"
   ipconfig1 = "ip=10.17.0.4${count.index + 1}/24"
-  sshkeys = join("\n", var.ssh_keys)
+  sshkeys   = join("\n", var.ssh_keys)
 }
 
 # resource "proxmox_vm_qemu" "kube-storage" {
--- vars.tf
+++ vars.tf
@@ -1,23 +1,33 @@
 variable "template_name" {
-    default = "ubuntu-2004-cloudinit-template"
+  default = "ubuntu-2204-cloudinit-template"
+}
+
+variable "proxmox_host" {
+  default = "ironman"
 }
 
 variable "kube_control_count" {
-    default = 1
+  default = 1
 }
 variable "kube_control_memory" {
-    default = 4096
+  default = 4096
+}
+variable "kube_control_cores" {
+  default = 2
 }
 variable "kube_control_disksize" {
-    default = "10G"
+  default = "10G"
 }
 
 variable "kube_node_count" {
-    default = 3
+  default = 2
 }
 variable "kube_node_memory" {
-    default = 4096
+  default = 10240
+}
+variable "kube_node_cores" {
+  default = 2
 }
 variable "kube_node_disksize" {
-    default = "10G"
+  default = "10G"
 }
